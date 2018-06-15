28d3733b0a68bc662740b664a5540571467b7a90
preethid <e0203237@u.nus.edu>
2018-06-15 13:53:46 +0800

    Second commit

--- app/src/main/java/com/example/preethidevarajan/helplahorbital/MainActivity.java
+++ app/src/main/java/com/example/preethidevarajan/helplahorbital/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.preethidevarajan.helplahorbital;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onLoginClick(View view) {
+        //what happens when they click Log In
+    }
+
+    public void onSignUpClick(View view) {
+        //what happend when they click sign up
+    }
 }
+
