2fcef02ee004e1a6d4155d252e7a7aae19c7791e
preethid <e0203237@u.nus.edu>
2018-07-02 09:47:34 +0800

    Milestone 2 random screens also included

--- app/src/main/java/com/example/preethidevarajan/helplahorbital/HomeActivity.java
+++ app/src/main/java/com/example/preethidevarajan/helplahorbital/HomeActivity.java
@@ -1,16 +1,21 @@
 package com.example.preethidevarajan.helplahorbital;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toolbar;
 
-public class HomeActivity extends AppCompatActivity{
-
+public class HomeActivity extends AppCompatActivity {
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+       // Toolbar toolbar = findViewById(R.id.toolbar);
+        // setSupportActionBar(toolbar);
 
-
+       // final ActionBar actionBar = getSupportActionBar();
+        // actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
