85dea233d87a9da3d7da510c0da08c2b1b8f0d6d
preethid <e0203237@u.nus.edu>
2018-07-08 16:14:36 +0800

    Sign up, Log in connected to Fire Base Home page has navigation drawer when swipe right Has new question floating point button

--- app/src/main/java/com/example/preethidevarajan/helplahorbital/MainActivity.java
+++ app/src/main/java/com/example/preethidevarajan/helplahorbital/MainActivity.java
@@ -2,18 +2,27 @@ package com.example.preethidevarajan.helplahorbital;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     EditText username;
     EditText password;
     Button sign_up;
     Button log_in;
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +35,48 @@ public class MainActivity extends AppCompatActivity {
         log_in = (Button) findViewById(R.id.button);
         sign_up = (Button) findViewById(R.id.button2);
 
+        //firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
         log_in.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //what happens when they click Log In
-                //If use hasnt signed up,
-                //Toast.makeText(MainActivity.this, "Account does not exist", Toast.LENGTH_LONG).show();
+                                      @Override
+                                      public void onClick(View view) {
+                                          //what happens when they click Log In
+                                          //If use hasnt signed up,
+                                          //Toast.makeText(MainActivity.this, "Account does not exist", Toast.LENGTH_LONG).show();
+                                          String email = username.getText().toString().trim();
+                                          String pass_word = password.getText().toString().trim();
 
-                openHomeActivity();
+                                          // Check if email is empty
+                                          if (TextUtils.isEmpty(email)) {
+                                              Toast.makeText(MainActivity.this, "Enter Email Address", Toast.LENGTH_SHORT).show();
+                                              return;
+                                          }
 
 
-            }
-        });
+                                          // check if password is empty
+                                          if (TextUtils.isEmpty(pass_word)) {
+                                              Toast.makeText(MainActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                                              return;
+                                          }
+
+                                          //authenticate user
+                                          auth.signInWithEmailAndPassword(email, pass_word)
+                                                  .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                                                      @Override
+                                                      public void onComplete(@NonNull Task<AuthResult> task) {
+                                                          if (!task.isSuccessful()) {
+                                                              // error occurred
+                                                              Toast.makeText(MainActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                                                          } else {
+                                                              openHomeActivity();
+                                                          }
+
+
+                                                      }
+                                                  });
+                                      }
+                                  });
 
         sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,11 +85,12 @@ public class MainActivity extends AppCompatActivity {
                 openSignUpActivity();
             }
         });
-
     }
 
+
+
     public void openHomeActivity() {
-        Intent intent = new Intent(this, HomeActivity.class);
+        Intent intent = new Intent(this, NavigationActivity.class);
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/preethidevarajan/helplahorbital/SignUpActivity.java
+++ app/src/main/java/com/example/preethidevarajan/helplahorbital/SignUpActivity.java
@@ -1,17 +1,28 @@
 package com.example.preethidevarajan.helplahorbital;
 
 import android.content.Intent;
-import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class SignUpActivity extends AppCompatActivity {
 
     EditText username;
     EditText password;
     Button create;
+    private FirebaseAuth mAuth;
+
 
 
     @Override
@@ -22,11 +33,50 @@ public class SignUpActivity extends AppCompatActivity {
         username = (EditText) findViewById(R.id.editText3);
         password = (EditText) findViewById(R.id.editText4);
         create = (Button) findViewById(R.id.button3);
+        //Firebase Auth Instance
+        mAuth = FirebaseAuth.getInstance();
 
         create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openHomeActivity();
+                String email = username.getText().toString().trim();
+                final String pass_word = password.getText().toString().trim();
+
+                //Check if email is empty
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(SignUpActivity.this, "Enter Email Address", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //Check if password is empty
+                if (TextUtils.isEmpty(pass_word)) {
+                    Toast.makeText(SignUpActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //check if password is alphanumeric
+                if(!pass_word.matches("^(?=.[A-Z])(?=.[0-9])(?=.*[a-z])[a-zA-Z0-9]+$")){
+                    Toast.makeText(SignUpActivity.this, "Password must contain one " +
+                            "upper and lower case letter and one number", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+
+                mAuth.createUserWithEmailAndPassword(email, pass_word).
+                addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()){
+                            Toast.makeText(SignUpActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            //signup successful, go to home activity
+                            openHomeActivity();
+                            finish();
+                        }
+                    }
+                });
+
             }
         });
 
