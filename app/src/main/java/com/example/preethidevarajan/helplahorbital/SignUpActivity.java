e9773d34613b80fbd562d0cfe2b4966a71f709ee
yoyotee <reachyogusvi@gmail.com>
2018-07-08 15:53:42 +0800

    Faculty acitivity and SoC modules Activity plus sign up/login and home page icon

--- app/src/main/java/com/example/preethidevarajan/helplahorbital/SignUpActivity.java
+++ app/src/main/java/com/example/preethidevarajan/helplahorbital/SignUpActivity.java
@@ -2,16 +2,20 @@ package com.example.preethidevarajan.helplahorbital;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    EditText username;
-    EditText password;
-    Button create;
+    private EditText username;
+    private EditText password;
+    private Button create;
+    private FirebaseAuth auth;
 
 
     @Override
@@ -19,22 +23,59 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        // initialise widgets
         username = (EditText) findViewById(R.id.editText3);
         password = (EditText) findViewById(R.id.editText4);
         create = (Button) findViewById(R.id.button3);
 
+        // Firebase Auth Instance
+        auth = FirebaseAuth.getInstance();
+
         create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openHomeActivity();
+                String email = username.getText().toString().trim();
+                String password = password.getText().toString().trim();
+                // Check if email is empty
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(SignupActivity.this, "Enter Email Address", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+// check if password is empty
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(SignupActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+//check if password is alphanumeric
+                if(!password.matches("^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])[a-zA-Z0-9]+$")){
+                    Toast.makeText(SignupActivity.this, "Password must contain one " +
+                            "upper and lower case letter and one number", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                // Set your own additional constraints
+
+                // Create a new user
+                auth.createUserWithEmailAndPassword(email, password).
+                        addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(SignupActivity.this,
+                                            "Authentication Failed", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // Signup successful, got to main activity
+                                    startActivity(new Intent(SignupActivity.this, MainActivity.class));
+                                    // End the activity
+                                    finish();
+                                }
+                            }
+                        });
             }
         });
 
 
-    }
-
-    public void openHomeActivity() {
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
     }
 }
